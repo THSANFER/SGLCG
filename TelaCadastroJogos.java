7d44b5a3daf1e7b47be622cd35314da748b11643
THSANFER <50412296+THSANFER@users.noreply.github.com>
2022-08-22 09:36:37 -0300

    Add files via upload

---
+++ TelaCadastroJogos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author Paulo Henrrique
+ */
+public class TelaCadastroJogos extends CadastrarJogos {
+
+    public TelaCadastroJogos() {
+    }
+    
+}
